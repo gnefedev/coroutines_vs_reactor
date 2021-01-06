f8d8849f61e85e9ce35b5f0ded5ee2475f16609e
gnefedev <georgynefedev@gmail.com>
2021-01-06 21:03:11 +0300

    java version

--- src/main/java/com/gnefedev/coroutines/vs/reactor/repos/AccountRepository.java
+++ src/main/java/com/gnefedev/coroutines/vs/reactor/repos/AccountRepository.java
@@ -2,18 +2,14 @@ package com.gnefedev.coroutines.vs.reactor.repos;
 
 import com.gnefedev.coroutines.vs.reactor.entities.Account;
 import org.springframework.data.repository.Repository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
-import java.util.Collection;
 
 public interface AccountRepository extends Repository<Account, Long> {
     Mono<Account> save(Account account);
 
     Mono<Void> transferAmount(long id, int version, BigDecimal toTransfer);
-
-    Flux<Account> findByIdIn(Collection<Long> ids);
 
     Mono<Account> findById(long id);
 }
--- src/main/java/com/gnefedev/coroutines/vs/reactor/services/Ledger.java
+++ src/main/java/com/gnefedev/coroutines/vs/reactor/services/Ledger.java
@@ -16,9 +16,7 @@ import reactor.util.retry.Retry;
 
 import java.math.BigDecimal;
 import java.time.Duration;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static com.gnefedev.coroutines.vs.reactor.util.LoggerHelper.withMDC;
 
@@ -39,37 +37,33 @@ public class Ledger {
                         log.warn("retry of transaction " + transactionKey);
                         return Mono.empty();
                     }
-                    return accountRepository.findByIdIn(List.of(fromAccountId, toAccountId))
-                            .collect(Collectors.toList())
-                            .flatMap(accounts -> {
-                                if (accounts.size() < 2) {
-                                    return Mono.error(new IllegalArgumentException("account not found"));
-                                }
-                                var fromAccount = accounts.stream().filter(account -> account.getId() == fromAccountId).findFirst().orElseThrow();
-                                var toAccount = accounts.stream().filter(account -> account.getId() == toAccountId).findFirst().orElseThrow();
-
-                                var transactionToInsert = Transaction.builder()
-                                        .amount(amountToTransfer)
-                                        .fromAccountId(fromAccountId)
-                                        .toAccountId(toAccountId)
-                                        .uniqueKey(transactionKey)
-                                        .build();
-                                if (fromAccount.getAmount().subtract(amountToTransfer).compareTo(BigDecimal.ZERO) < 0 || toAccount.getAmount().add(amountToTransfer).compareTo(BigDecimal.ZERO) < 0) {
-                                    return Mono.error(new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "can't transfer, not enough money"));
-                                }
-                                return transactionalOperator.transactional(
-                                        transactionRepository.save(transactionToInsert)
-                                                .then(accountRepository.transferAmount(fromAccount.getId(), fromAccount.getVersion(), amountToTransfer.negate()))
-                                                .then(accountRepository.transferAmount(toAccount.getId(), toAccount.getVersion(), amountToTransfer))
-                                ).onErrorResume(error -> {
-                                    //transaction was inserted on parallel transaction, we may return success response
-                                    if (error instanceof DataIntegrityViolationException && error.getMessage().contains("TRANSACTION_UNIQUE_KEY")) {
-                                        return Mono.empty();
-                                    } else {
-                                        return Mono.error(error);
-                                    }
-                                });
-                            });
+                    return accountRepository.findById(fromAccountId)
+                            .switchIfEmpty(Mono.error(new IllegalArgumentException("account not found")))
+                            .flatMap(fromAccount -> accountRepository.findById(toAccountId)
+                                    .switchIfEmpty(Mono.error(new IllegalArgumentException("account not found")))
+                                    .flatMap(toAccount -> {
+                                        var transactionToInsert = Transaction.builder()
+                                                .amount(amountToTransfer)
+                                                .fromAccountId(fromAccountId)
+                                                .toAccountId(toAccountId)
+                                                .uniqueKey(transactionKey)
+                                                .build();
+                                        if (fromAccount.getAmount().subtract(amountToTransfer).compareTo(BigDecimal.ZERO) < 0 || toAccount.getAmount().add(amountToTransfer).compareTo(BigDecimal.ZERO) < 0) {
+                                            return Mono.error(new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "can't transfer, not enough money"));
+                                        }
+                                        return transactionalOperator.transactional(
+                                                transactionRepository.save(transactionToInsert)
+                                                        .then(accountRepository.transferAmount(fromAccount.getId(), fromAccount.getVersion(), amountToTransfer.negate()))
+                                                        .then(accountRepository.transferAmount(toAccount.getId(), toAccount.getVersion(), amountToTransfer))
+                                        ).onErrorResume(error -> {
+                                            //transaction was inserted on parallel transaction, we may return success response
+                                            if (error instanceof DataIntegrityViolationException && error.getMessage().contains("TRANSACTION_UNIQUE_KEY")) {
+                                                return Mono.empty();
+                                            } else {
+                                                return Mono.error(error);
+                                            }
+                                        });
+                                    }));
                 }))
                 .retryWhen(Retry.backoff(3, Duration.ofMillis(1))
                         .filter(OptimisticLockException.class::isInstance)
@@ -82,47 +76,44 @@ public class Ledger {
     }
 
     public Mono<Void> transferParallel(String transactionKey, long fromAccountId, long toAccountId, BigDecimal amountToTransfer) {
-        return transactionRepository.findByUniqueKey(transactionKey)
-                .map(Optional::of)
-                .defaultIfEmpty(Optional.empty())
-                .zipWith(accountRepository.findByIdIn(List.of(fromAccountId, toAccountId))
-                        .collect(Collectors.toList()))
-                .flatMap(withMDC(pair -> {
-                    var foundTransaction = pair.getT1();
-                    if (foundTransaction.isPresent()) {
-                        log.warn("retry of transaction " + transactionKey);
-                        return Mono.empty();
-                    }
-
-                    var accounts = pair.getT2();
-                    if (accounts.size() < 2) {
-                        return Mono.error(new IllegalArgumentException("account not found"));
-                    }
-                    var fromAccount = accounts.stream().filter(account -> account.getId() == fromAccountId).findFirst().orElseThrow();
-                    var toAccount = accounts.stream().filter(account -> account.getId() == toAccountId).findFirst().orElseThrow();
-
-                    var transactionToInsert = Transaction.builder()
-                            .amount(amountToTransfer)
-                            .fromAccountId(fromAccountId)
-                            .toAccountId(toAccountId)
-                            .uniqueKey(transactionKey)
-                            .build();
-                    if (fromAccount.getAmount().subtract(amountToTransfer).compareTo(BigDecimal.ZERO) < 0 || toAccount.getAmount().add(amountToTransfer).compareTo(BigDecimal.ZERO) < 0) {
-                        return Mono.error(new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "can't transfer, not enough money"));
-                    }
-                    return transactionalOperator.transactional(
-                            transactionRepository.save(transactionToInsert)
-                                    .then(accountRepository.transferAmount(fromAccount.getId(), fromAccount.getVersion(), amountToTransfer.negate()))
-                                    .then(accountRepository.transferAmount(toAccount.getId(), toAccount.getVersion(), amountToTransfer))
-                    ).onErrorResume(error -> {
-                        //transaction was inserted on parallel transaction, we may return success response
-                        if (error instanceof DataIntegrityViolationException && error.getMessage().contains("TRANSACTION_UNIQUE_KEY")) {
-                            return Mono.empty();
-                        } else {
-                            return Mono.error(error);
-                        }
-                    });
-                }))
+        return Mono.zip(
+                transactionRepository.findByUniqueKey(transactionKey)
+                        .map(Optional::of)
+                        .defaultIfEmpty(Optional.empty()),
+                accountRepository.findById(fromAccountId)
+                        .switchIfEmpty(Mono.error(new IllegalArgumentException("account not found"))),
+                accountRepository.findById(toAccountId)
+                        .switchIfEmpty(Mono.error(new IllegalArgumentException("account not found")))
+        ).flatMap(withMDC(fetched -> {
+            var foundTransaction = fetched.getT1();
+            var fromAccount = fetched.getT2();
+            var toAccount = fetched.getT3();
+            if (foundTransaction.isPresent()) {
+                log.warn("retry of transaction " + transactionKey);
+                return Mono.empty();
+            }
+            var transactionToInsert = Transaction.builder()
+                    .amount(amountToTransfer)
+                    .fromAccountId(fromAccountId)
+                    .toAccountId(toAccountId)
+                    .uniqueKey(transactionKey)
+                    .build();
+            if (fromAccount.getAmount().subtract(amountToTransfer).compareTo(BigDecimal.ZERO) < 0 || toAccount.getAmount().add(amountToTransfer).compareTo(BigDecimal.ZERO) < 0) {
+                return Mono.error(new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "can't transfer, not enough money"));
+            }
+            return transactionalOperator.transactional(
+                    transactionRepository.save(transactionToInsert)
+                            .then(accountRepository.transferAmount(fromAccount.getId(), fromAccount.getVersion(), amountToTransfer.negate()))
+                            .then(accountRepository.transferAmount(toAccount.getId(), toAccount.getVersion(), amountToTransfer))
+            ).onErrorResume(error -> {
+                //transaction was inserted on parallel transaction, we may return success response
+                if (error instanceof DataIntegrityViolationException && error.getMessage().contains("TRANSACTION_UNIQUE_KEY")) {
+                    return Mono.empty();
+                } else {
+                    return Mono.error(error);
+                }
+            });
+        }))
                 .retryWhen(Retry.backoff(3, Duration.ofMillis(1))
                         .filter(OptimisticLockException.class::isInstance)
                         .onRetryExhaustedThrow((__, retrySignal) -> retrySignal.failure())
