44ef01f4df04ca3d43441f79e25af1c02e7017cf
gnefedev <georgynefedev@gmail.com>
2021-01-16 11:33:43 +0300

    rename java files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     application
-    java
+    kotlin("jvm") version "1.4.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.4.21"
 }
 
 repositories {
     jcenter()
 }
+
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_15.toString()
+}
+
 
 tasks.test {
     useJUnitPlatform {
@@ -18,13 +27,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.r2dbc:r2dbc-pool")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework:spring-aspects")
     runtimeOnly("io.r2dbc:r2dbc-h2")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.4.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("io.github.microutils:kotlin-logging:1.4.1")
+    implementation("com.michael-bull.kotlin-retry:kotlin-retry:1.0.5")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    compileOnly("org.projectlombok:lombok:1.18.16")
-    annotationProcessor("org.projectlombok:lombok:1.18.16")
-
-    testCompileOnly("org.projectlombok:lombok:1.18.16")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.16")
 }
